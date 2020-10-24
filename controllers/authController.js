fefaa35a656b72d624271fe860b33fe48a7a1cc1
Collins Thomas <thomascollins582@gmail.com>
2020-10-24 19:45:04 +0100

    Login route in progress

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+
+const signToken = function (id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const userObj = {
@@ -15,11 +22,34 @@ exports.signUp = catchAsync(async (req, res, next) => {
   };
 
   const newUser = await User.create(userObj);
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
     message: 'User created successfully',
     data: {
+      token,
       data: newUser,
     },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password, username } = req.body;
+
+  if ((email || username) && password) {
+    const user = await User.findOne({ $or: [{ email }, { username }] }).select(
+      '+password'
+    );
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Login successfully',
+      data: {
+        data: user,
+      },
+    });
+  } else {
+    return next(new AppError('Please provide email or username and password'));
+  }
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.route('/signup').post(authController.signUp);
+router.route('/login').post(authController.login);
 
 router.route('/').get(userController.getAllUsers);
 
