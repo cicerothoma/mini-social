717a609fc1883d84f11a22dc608c0fccd87a1a3f
Collins Thomas <thomascollins582@gmail.com>
2020-10-31 10:16:44 +0100

    Modified Error Controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,12 @@
 const AppError = require('./../utils/appError');
 
 const sendErrorDev = (err, req, res, next) => {
-  if (req.originalUrl.startsWith('/api')) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack,
-      error: err,
-    });
-  }
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
 };
 
 module.exports = (err, req, res, next) => {
