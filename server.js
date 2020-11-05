91e570dad23579ac16345fc6205e7efb9fce508e
Collins Thomas <thomascollins582@gmail.com>
2020-11-05 22:25:24 +0100

    Added Socket.io events for messages

--- server.js
+++ server.js
@@ -19,3 +19,29 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  //Get the chatID of the user and join in a room of the same chatID
+  chatID = socket.handshake.query.chatID;
+  socket.join(chatID);
+
+  //Leave the room if the user closes the socket
+  socket.on('disconnect', () => {
+    socket.leave(chatID);
+  });
+
+  //Send message to only a particular user
+  socket.on('send_message', (message) => {
+    receiverChatID = message.receiverChatID;
+    senderChatID = message.senderChatID;
+    content = message.content;
+
+    //Send message to only that particular room
+    socket.in(receiverChatID).emit('receive_message', {
+      content: content,
+      senderChatID: senderChatID,
+      receiverChatID: receiverChatID,
+    });
+  });
+});
