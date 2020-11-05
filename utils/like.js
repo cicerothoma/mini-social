9992a2f54c17018f8fa77bedc9fbf0a018b2423d
Collins Thomas <thomascollins582@gmail.com>
2020-11-05 23:19:31 +0100

    Modified Like Utility Function to return object

--- utils/like.js
+++ utils/like.js
@@ -5,11 +5,13 @@ module.exports = async (doc, req, res) => {
     doc.likes.push(req.user._id);
     await doc.update({ likes: doc.likes });
     sendResponse({ docLiked: true }, res, 200, { message: 'Document Liked' });
+    return { docLiked: true };
   } else {
     doc.likes.splice(doc.likes.indexOf(req.user._id), 1);
     await doc.update({ likes: doc.likes });
     sendResponse({ docLiked: false }, res, 200, {
       message: 'Document Unliked',
     });
+    return { docLiked: false };
   }
 };
