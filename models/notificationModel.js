ddeba60ff83a439b4d8fa1b80442afe727b085c4
Collins Thomas <thomascollins582@gmail.com>
2020-11-06 20:08:43 +0100

    Modified notification model

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,32 +1,47 @@
 const mongoose = require('mongoose');
 
-const notificationSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  }, // Notification creator
-  receiver: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Ids of the receivers of the notification
-  message: String, // any description of the notification message
-  document: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  readBy: [
-    {
-      readerID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      readAt: {
-        type: Date,
-        default: Date.now,
-      },
+const notificationSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }, // Notification creator
+    receiver: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Ids of the receivers of the notification
+    message: String, // any description of the notification message
+    document: {
+      type: mongoose.Schema.Types.ObjectId,
     },
-  ],
-  created_at: {
-    type: Date,
-    default: Date.now,
+    type: {
+      type: String,
+      enum: ['like', 'comment', 'reply'],
+      message: ['type should be either like, comment or reply'],
+    },
+    readBy: [
+      {
+        readerID: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        readAt: {
+          type: Date,
+          default: Date.now,
+          immutable: true,
+        },
+      },
+    ],
+    created_at: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+    endPoint: String,
   },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    strict: true,
+  }
+);
 
 const Notification = mongoose.model(
   'Notification',
