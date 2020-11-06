b974db6afa1dccd12f8b0e51c59114956b8f54fc
Collins Thomas <thomascollins582@gmail.com>
2020-11-06 20:11:58 +0100

    Added optional options argument

--- utils/notify.js
+++ utils/notify.js
@@ -5,11 +5,15 @@ module.exports = async (
   senderID,
   receiverId,
   message,
+  options = { type: false, affectedDoc: false, endPoint: false },
   model = Notification
 ) => {
   await model.create({
     sender: senderID,
     receiver: receiverId,
     message,
+    type: options.type ? options.type : undefined,
+    document: options.affectedDoc ? options.affectedDoc : undefined,
+    endPoint: options.endPoint ? options.endPoint : undefined,
   });
 };
