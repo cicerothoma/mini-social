0ed8cd12f97826cf89a2cfa27d80ed15bbd10ade
Collins Thomas <thomascollins582@gmail.com>
2020-11-13 15:51:38 +0100

    Removed deprecated update function and used updateOne instead

--- utils/like.js
+++ utils/like.js
@@ -3,12 +3,12 @@ const sendResponse = require('./sendResponse');
 module.exports = async (doc, req, res) => {
   if (!doc.likes.includes(req.user._id)) {
     doc.likes.push(req.user._id);
-    await doc.update({ likes: doc.likes });
+    await doc.updateOne({ likes: doc.likes });
     sendResponse({ docLiked: true }, res, 200, { message: 'Document Liked' });
     return { docLiked: true };
   } else {
     doc.likes.splice(doc.likes.indexOf(req.user._id), 1);
-    await doc.update({ likes: doc.likes });
+    await doc.updateOne({ likes: doc.likes });
     sendResponse({ docLiked: false }, res, 200, {
       message: 'Document Unliked',
     });
