315b252fa03907c4af680b4401e3304fcbb09e74
Collins Thomas <thomascollins582@gmail.com>
2020-11-19 17:21:35 +0100

    Added Function to upload file from buffer

---
+++ utils/uploadFromBuffer.js
@@ -0,0 +1,34 @@
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFromBuffer = (
+  req,
+  buffer,
+  options = {
+    folder: `${req.user._id}/media`,
+    timeout: 600000,
+  }
+) => {
+  return new Promise((resolve, reject) => {
+    const cld_upload_stream = cloudinary.uploader.upload_stream(
+      options,
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+
+    streamifier.createReadStream(buffer).pipe(cld_upload_stream);
+  });
+};
+
+module.exports = uploadFromBuffer;
