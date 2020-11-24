0d8eae65074814a62024c2320c5d4f7b1a0a572e
Collins Thomas <thomascollins582@gmail.com>
2020-11-24 13:13:06 +0100

    Restructured Middleware Placement to Avoid repeatition

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,48 +8,36 @@ router.route('/signup').post(authController.signUp);
 router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:resetToken').patch(authController.resetPassword);
-router
-  .route('/sendResetEmailToken')
-  .post(authController.protect, authController.sendResetEmailToken);
 router.route('/resetEmail/:resetToken').patch(authController.resetEmail);
 
-router
-  .route('/follow')
-  .get(authController.protect, userController.getUsersToFollow);
-router
-  .route('/follow/:toFollowID')
-  .patch(authController.protect, userController.follow);
+router.use(authController.protect);
+
+router.route('/sendResetEmailToken').post(authController.sendResetEmailToken);
+
+router.route('/follow').get(userController.getUsersToFollow);
+router.route('/follow/:toFollowID').patch(userController.follow);
 
 router
   .route('/')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin'),
-    userController.getAllUsers
-  );
+  .get(authController.restrictTo('admin'), userController.getAllUsers);
 
-router
-  .route('/updateMyPassword')
-  .patch(authController.protect, authController.updatePassword);
+router.route('/updateMyPassword').patch(authController.updatePassword);
 
 router
   .route('/updateMe')
   .patch(
-    authController.protect,
     userController.getImage,
     userController.uploadProfileImage,
     userController.updateProfile
   );
 
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/:id')
-  .delete(
-    authController.protect,
-    authController.restrictTo('admin'),
-    userController.deleteUser
-  )
+  .delete(userController.deleteUser)
   .patch(userController.updateUser);
 
 module.exports = router;
