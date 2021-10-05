7452b4b1f5af78fb9ad6627590ea2d1797ab2d42
Collins Thomas <thomascollins582@gmail.com>
2021-10-05 17:13:38 +0100

    replaced gmail with sendgrid for sending mails

--- utils/email.js
+++ utils/email.js
@@ -3,16 +3,21 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1) Create Transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.GMAIL_PASSWORD,
-    },
+    // service: 'Gmail',
+    // auth: {
+    //   user: process.env.GMAIL_USERNAME,
+    //   pass: process.env.GMAIL_PASSWORD,
+    // },
     // Remember to active the "less secure app" option for Gmail to work with nodemailer
+    service: 'SendGrid',
+    auth: {
+      user: process.env.SENDGRID_USERNAME,
+      pass: process.env.SENDGRID_PASSWORD,
+    },
   });
   // 2) Define Options
   const mailOptions = {
-    from: 'Live And Learn <liveandlearncoding@gmail.com>',
+    from: `Thomas Collins <${process.env.EMAIL_FROM}>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
