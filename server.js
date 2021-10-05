cdb5eab24d0a780f028dead40d6cf439d5a511d7
Collins Thomas <thomascollins582@gmail.com>
2021-10-05 17:31:29 +0100

    removed unused code

--- server.js
+++ server.js
@@ -28,30 +28,6 @@ const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`App running on port ${port}`));
 
-io.on('connection', (socket) => {
-  //Get the chatID of the user and join in a room of the same chatID
-  chatID = socket.handshake.query.chatID;
-  socket.join(chatID);
-
-  //Leave the room if the user closes the socket
-  socket.on('disconnect', () => {
-    socket.leave(chatID);
-  });
-
-  //Send message to only a particular user
-  socket.on('send_message', (message) => {
-    receiverChatID = message.receiverChatID;
-    senderChatID = message.senderChatID;
-    content = message.content;
-
-    //Send message to only that particular room
-    socket.in(receiverChatID).emit('receive_message', {
-      content: content,
-      senderChatID: senderChatID,
-      receiverChatID: receiverChatID,
-    });
-  });
-});
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION!! Closing Server then shutting down');
